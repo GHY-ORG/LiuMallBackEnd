d14f89ba872bf87357629e824d4d352da88f7be7
ddreset <zhangyd96@gmail.com>
2018-05-29 23:15:19 +0800

    [INI] initial

---
+++ src/main/java/cn/ghy/liumall/LiumallApplication.java
@@ -0,0 +1,12 @@
+package cn.ghy.liumall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiumallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiumallApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/ghy/liumall/LiumallApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.ghy.liumall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiumallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
